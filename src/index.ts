d9631460032940eb621ba5553ce68605a48b61bd
Sushruth Sastry <shastry.sushruth@gmail.com>
2025-03-28 11:12:08 -0700

    feat: contracts install command

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
-import { OneFeCommonConfig, OneFeBaseConfiguration } from '@devhub/cli';
+import { OneFeCommonConfig, OneFeBaseConfigurationObject } from '@devhub/cli';
 
 import ky from 'ky';
 
-export const getBaseConfig: OneFeBaseConfiguration = async () => {
-  const commonConfig = await ky
-    .get(
-      'https://cdn.jsdelivr.net/gh/docusign/mock-cdn-assets/common-configs/integration.json',
-    )
-    .json<OneFeCommonConfig>();
+const environments = ['development', 'integration', 'production'] as const;
 
-  return {
-    environments: {
-      integration: {
-        commonConfig,
-        shellBaseUrl: 'https://example.com', // Add the appropriate URL here
-        serverBaseUrl: 'https://example.com', // Add the appropriate URL here
-      },
-    },
-    baththubUrl: '',
-  };
+export type Environments = (typeof environments)[number];
+
+const configUrls: Record<Environments, string> = {
+  development:
+    'https://cdn.jsdelivr.net/gh/docusign/mock-cdn-assets/common-configs/development.json',
+  integration:
+    'https://cdn.jsdelivr.net/gh/docusign/mock-cdn-assets/common-configs/integration.json',
+  production:
+    'https://cdn.jsdelivr.net/gh/docusign/mock-cdn-assets/common-configs/production.json',
 };
+
+function getCommonConfig(env: Environments) {
+  return ky.get(configUrls[env]).json<OneFeCommonConfig>();
+}
+
+export async function getBaseConfig(): Promise<OneFeBaseConfigurationObject> {
+  const baseConfig: OneFeBaseConfigurationObject = {
+    environments: {},
+    bathtubUrl: 'http://localhost:3001/bathtub',
+  };
+
+  for (const env of environments) {
+    baseConfig.environments[env] = {
+      commonConfig: await getCommonConfig(env),
+      shellBaseUrl: 'http://localhost:3001',
+      serverBaseUrl: 'http://localhost:3001',
+    };
+  }
+
+  return baseConfig;
+}
